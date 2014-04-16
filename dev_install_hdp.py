321847937dca4a375d51b8d8e7a5bb5fa5c02a65
jmmnn <jmmnn@yahoo.com>
2014-04-16 19:46:21 +0200

    removed unnecessary variables

--- dev_install_hdp.py
+++ dev_install_hdp.py
@@ -7,13 +7,24 @@ import sys
 # Variables
 
 #List commands to execute here.
+
+#firewall and clock
 NTPD = "sudo chkconfig ntpd on"
 IPTABLES_OFF = "sudo chkconfig iptables off"
 IPTABLES_STOP = "sudo /etc/init.d/iptables stop"
 NTPD_START = "sudo service ntpd start"
 
+#install ambari
+CD = "cd /etc/yum.repos.d/"
+GET_AMBARI_REPO = "wget http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.5.0/ambari.repo"
+INSTALL_AMBARI = "yum install ambari-server"
+SETUP_AMBARI = ambari-server setup
+START_AMBARI = ambari-server start
+
+
+
 #order them in sequence
-cmds = [NTPD, IPTABLES_OFF, IPTABLES_STOP, NTPD_START]
+cmds = [NTPD, IPTABLES_OFF, IPTABLES_STOP, NTPD_START, CD, GET_AMBARI_REPO, SETUP_AMBARI, START_AMBARI]
 
 #Iterates over list, running statements for each item in the list
 #Note, that whitespace is absolutely critical and that a consistent indent must be maintained for the code to work properly
